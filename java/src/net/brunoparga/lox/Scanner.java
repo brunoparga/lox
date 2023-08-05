d2e56c1f8f5a273e12918648eddcc9a4905a040e
Bruno Parga <brunoparga@gmail.com>
2023-08-05 19:19:31 +0200

    [java] 4.5.1 - Scan single-character tokens

--- java/src/net/brunoparga/lox/Scanner.java
+++ java/src/net/brunoparga/lox/Scanner.java
@@ -34,6 +34,35 @@ public class Scanner {
   }
 
   private void scanToken() {
-    // stub
+    char c = advance();
+    switch (c) {
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break;
+
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
+    }
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 }
