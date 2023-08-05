5e9ec3a2518da38803734441903f3d7d564e8cc1
Bruno Parga <brunoparga@gmail.com>
2023-08-05 18:25:13 +0200

    [java] 4.4 - Stub the scanner

--- java/src/net/brunoparga/lox/Scanner.java
+++ java/src/net/brunoparga/lox/Scanner.java
@@ -1,17 +1,39 @@
 package net.brunoparga.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static net.brunoparga.lox.TokenType.*;
 
 public class Scanner {
-    private final String source;
-    private final List<Token> tokens = new ArrayList<>();
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
 
-    Scanner(String source) {
-        this.source = source;
+  Scanner(String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // We are at the beginning of the next lexeme
+      start = current;
+      scanToken();
     }
 
-    List<Token> scanTokens() {
-        return tokens;
-    }
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private void scanToken() {
+    // stub
+  }
 }
