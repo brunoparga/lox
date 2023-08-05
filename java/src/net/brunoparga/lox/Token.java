a87392b891e1ab6b661a159ef210226b5f60206b
Bruno Parga <brunoparga@gmail.com>
2023-08-05 15:06:38 +0200

    [java] 4.2 - Implement Tokens and TokenTypes

--- java/src/net/brunoparga/lox/Token.java
+++ java/src/net/brunoparga/lox/Token.java
@@ -1,5 +1,19 @@
 package net.brunoparga.lox;
 
-public class Token {
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
 
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
 }
