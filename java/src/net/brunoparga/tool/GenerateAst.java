f0fc5991a9b4d4883b9a7c2b1803055a1f6e6613
Bruno Parga <brunoparga@gmail.com>
2023-08-07 09:54:03 +0200

    [java] 5.3 Use Visitor pattern vs. Expr Problem

--- java/src/net/brunoparga/tool/GenerateAst.java
+++ java/src/net/brunoparga/tool/GenerateAst.java
@@ -33,11 +33,17 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
+    // The AST classes
     for (String type : types) {
       String className = type.split(":")[0].trim();
       String fields = type.split(":")[1].trim();
       defineType(writer, baseName, className, fields);
     }
+
+    // The base accept() method
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
     writer.println("}");
     writer.close();
@@ -61,6 +67,15 @@ public class GenerateAst {
 
     writer.println("    }");
 
+    // Visitor pattern
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
+    writer.println("    }");
+
+
+
     // Fields
     writer.println();
     for (String field : fields) {
@@ -68,5 +83,21 @@ public class GenerateAst {
     }
 
     writer.println("  }\n");
+  }
+
+  private static void defineVisitor(
+    PrintWriter writer, String baseName, List<String> types
+  ) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" +
+        typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
+    writer.println();
+
   }
 }
