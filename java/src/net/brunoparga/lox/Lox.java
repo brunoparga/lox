a005afb02c4118e97a68e1559b1af7491ee98a5e
Bruno Parga <brunoparga@gmail.com>
2023-08-10 17:37:38 +0200

    [java] 8.1 Interpret expr and print stmts

--- java/src/net/brunoparga/lox/Lox.java
+++ java/src/net/brunoparga/lox/Lox.java
@@ -56,13 +56,13 @@ public class Lox {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
     Parser parser = new Parser(tokens);
-    Expr expression = parser.parse();
+    List<Stmt> statements = parser.parse();
 
     // Stop if there was a syntax error
     if (hadError)
       return;
 
-    interpreter.interpret(expression);
+    interpreter.interpret(statements);
   }
 
   private static void report(int line, String where, String message) {
--- java/src/net/brunoparga/lox/Parser.java
+++ java/src/net/brunoparga/lox/Parser.java
@@ -1,5 +1,6 @@
 package net.brunoparga.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static net.brunoparga.lox.TokenType.*;
@@ -15,12 +16,32 @@ class Parser {
     this.tokens = tokens;
   }
 
-  Expr parse() {
-    try {
-      return expression();
-    } catch (ParseError error) {
-      return null;
+  List<Stmt> parse() {
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
+      statements.add(statement());
     }
+
+    return statements;
+  }
+
+  private Stmt statement() {
+    if (match(PRINT))
+      return printStatement();
+
+    return expressionStatement();
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Print(value);
+  }
+
+  private Stmt expressionStatement() {
+    Expr expr = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Expression(expr);
   }
 
   private Expr expression() {
