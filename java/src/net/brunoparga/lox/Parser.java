d5616764dd24d1e1e348f8afbd257a1b5cebf8f9
Bruno Parga <brunoparga@gmail.com>
2023-08-16 16:47:16 +0100

    [java] 8.5.2 Parse and interpret blocks

--- java/src/net/brunoparga/lox/Parser.java
+++ java/src/net/brunoparga/lox/Parser.java
@@ -50,8 +50,12 @@ class Parser {
   }
 
   private Stmt statement() {
-    if (match(PRINT))
+    if (match(PRINT)) {
       return printStatement();
+    }
+    if (match(LEFT_BRACE)) {
+      return new Stmt.Block(block());
+    }
 
     return expressionStatement();
   }
@@ -66,6 +70,17 @@ class Parser {
     Expr expr = expression();
     consume(SEMICOLON, "Expect ';' after value.");
     return new Stmt.Expression(expr);
+  }
+
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while(!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after block.");
+    return statements;
   }
 
   private Expr expression() {
