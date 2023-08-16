af217dcb43db3f19635ab24d591622ef4277f7d1
Bruno Parga <brunoparga@gmail.com>
2023-08-16 20:17:22 +0100

    [java] 9.2 Parse if statements

--- java/src/net/brunoparga/lox/Parser.java
+++ java/src/net/brunoparga/lox/Parser.java
@@ -50,6 +50,9 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(IF)) {
+      return ifStatement();
+    }
     if (match(PRINT)) {
       return printStatement();
     }
@@ -58,6 +61,20 @@ class Parser {
     }
 
     return expressionStatement();
+  }
+
+  private Stmt ifStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'if'.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
   }
 
   private Stmt printStatement() {
