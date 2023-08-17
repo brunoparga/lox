6f52760c4f8ea1a90629159b0ddcae03010d3b27
Bruno Parga <brunoparga@gmail.com>
2023-08-17 16:24:26 +0100

    [java] 9.3 Parse logical operators

--- java/src/net/brunoparga/lox/Parser.java
+++ java/src/net/brunoparga/lox/Parser.java
@@ -105,7 +105,7 @@ class Parser {
   }
 
   private Expr assignment() {
-    Expr expr = equality();
+    Expr expr = or();
 
     if (match(EQUAL)) {
       Token equals = previous();
@@ -117,6 +117,30 @@ class Parser {
       }
 
       error(equals, "Invalid assignment target.");
+    }
+
+    return expr;
+  }
+
+  private Expr or() {
+    Expr expr = and();
+
+    while (match(OR)) {
+      Token operator = previous();
+      Expr right = and();
+      expr = new Expr.Logical(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr and() {
+    Expr expr = equality();
+
+    while (match(AND)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Binary(expr, operator, right);
     }
 
     return expr;
--- java/src/net/brunoparga/tool/GenerateAst.java
+++ java/src/net/brunoparga/tool/GenerateAst.java
@@ -15,7 +15,7 @@ public class GenerateAst {
     String outputDir = args[0];
     defineAst(outputDir, "Expr",
         Arrays.asList("Assign: Token name, Expr value", "Binary: Expr left, Token operator, Expr right", "Grouping: Expr expression",
-            "Literal: Object value", "Unary: Token operator, Expr right", "Variable: Token name"));
+            "Literal: Object value", "Logical: Expr left, Token operator, Expr right", "Unary: Token operator, Expr right", "Variable: Token name"));
 
     defineAst(outputDir, "Stmt",
         Arrays.asList("Block: List<Stmt> statements", "Expression: Expr expression", "If: Expr condition, Stmt thenBranch, Stmt elseBranch", "Print: Expr expression", "Var: Token name, Expr initializer"));
