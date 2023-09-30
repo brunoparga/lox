fe2bda3e8d413b276e0795c2296d57398a9643cc
Bruno Parga <brunoparga@gmail.com>
2023-09-30 18:47:00 +0400

    [C] 15.3 Implement arithmetic operators

--- c/chunk.h
+++ c/chunk.h
@@ -5,8 +5,13 @@
 #include "value.h"
 
 typedef enum {
+  OP_ADD,
   OP_CONSTANT,
+  OP_DIVIDE,
+  OP_MULTIPLY,
+  OP_NEGATE,
   OP_RETURN,
+  OP_SUBTRACT,
 } Opcode;
 
 typedef struct {
--- c/vm.c
+++ c/vm.c
@@ -33,6 +33,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -47,21 +53,38 @@ static InterpretResult run() {
 #endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+      case OP_ADD:
+        BINARY_OP(+);
+        break;
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
         push(constant);
         break;
       }
+      case OP_DIVIDE:
+        BINARY_OP(/);
+        break;
+      case OP_MULTIPLY:
+        BINARY_OP(*);
+        break;
+      case OP_NEGATE:
+        push(-pop());
+        break;
       case OP_RETURN: {
         printValue(pop());
         printf("\n");
         return INTERPRET_OK;
+      }
+      case OP_SUBTRACT: {
+        BINARY_OP(-);
+        break;
       }
     }
   }
 
   #undef READ_BYTE
   #undef READ_CONSTANT
+  #undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
