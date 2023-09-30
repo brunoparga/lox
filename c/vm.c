e17e8c3d80af112892a4682f38f2f13df7a7b611
Bruno Parga <brunoparga@gmail.com>
2023-09-30 14:39:01 +0400

    [C] 15.2 Implement the VM stack

--- c/vm.c
+++ c/vm.c
@@ -3,16 +3,31 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "value.h"
 #include "vm.h"
 
 VM vm;
 
-void initVM() {
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
 
+void initVM() {
+  resetStack();
 }
 
 void freeVM() {
 
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -21,17 +36,25 @@ static InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
     }
