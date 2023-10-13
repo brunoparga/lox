c2f420e89b9575d287b478c3013f06ff42143b4c
Bruno Parga <brunoparga@gmail.com>
2023-10-13 07:54:24 +0400

    [C] 24.1 Represent function objects

--- c/object.c
+++ c/object.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "chunk.h"
 #include "memory.h"
 #include "object.h"
 #include "table.h"
@@ -17,6 +18,14 @@ static Obj *allocateObject(size_t size, ObjType type) {
   object->next = vm.objects;
   vm.objects = object;
   return object;
+}
+
+ObjFunction *newFunction() {
+  ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+  function->arity = 0;
+  function->name = NULL;
+  initChunk(&function->chunk);
+  return function;
 }
 
 static ObjString *allocateString(char *chars, int length, uint32_t hash) {
@@ -60,8 +69,15 @@ ObjString *copyString(const char *chars, int length) {
   return allocateString(heapChars, length, hash);
 }
 
+static void printFunction(ObjFunction *function) {
+  printf("<fn %s>", function->name->chars);
+}
+
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+  case OBJ_FUNCTION:
+    printFunction(AS_FUNCTION(value));
+    break;
   case OBJ_STRING:
     printf("%s", AS_CSTRING(value));
     break;
