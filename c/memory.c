804be78e1839c589b0f9344d20380646a7bf0a7b
Bruno Parga <brunoparga@gmail.com>
2023-10-14 06:15:45 +0400

    [C] 26.2 Log garbage collection

--- c/memory.c
+++ c/memory.c
@@ -3,7 +3,18 @@
 #include "memory.h"
 #include "vm.h"
 
+#ifdef DEBUG_LOG_GC
+#include "debug.h"
+#include <stdio.h>
+#endif
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
+  if (newSize > oldSize) {
+#ifdef DEBUG_STRESS_GC
+    collectGarbage();
+#endif
+  }
+
   if (newSize == 0) {
     free(pointer);
     return NULL;
@@ -16,6 +27,9 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
 }
 
 static void freeObject(Obj *object) {
+#ifdef DEBUG_LOG_GC
+  printf("%p free type %d\n", (void *)object, object->type);
+#endif
   switch (object->type) {
   case OBJ_CLOSURE: {
     ObjClosure *closure = (ObjClosure *)object;
@@ -42,6 +56,16 @@ static void freeObject(Obj *object) {
     FREE(ObjUpvalue, object);
     break;
   }
+}
+
+void collectGarbage() {
+#ifdef DEBUG_LOG_GC
+  printf("-- gc begin\n");
+#endif
+
+#ifdef DEBUG_LOG_GC
+  printf("-- gc end\n");
+#endif
 }
 
 void freeObjects() {
