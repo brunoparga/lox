5391024f43cba01527a128f9f7e1676a2c7a776f
Bruno Parga <brunoparga@gmail.com>
2023-10-14 06:31:34 +0400

    [C] 26.3 Mark "root" objects within direct reach

--- c/memory.c
+++ c/memory.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 
+#include "compiler.h"
 #include "memory.h"
+#include "object.h"
+#include "value.h"
 #include "vm.h"
 
 #ifdef DEBUG_LOG_GC
@@ -24,6 +27,23 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
   if (result == NULL)
     exit(1);
   return result;
+}
+
+void markObject(Obj *object) {
+  if (object == NULL)
+    return;
+#ifdef DEBUG_LOG_GC
+  printf("%p mark ", (void *)object);
+  printValue(OBJ_VAL(object));
+  printf("\n");
+#endif
+
+  object->isMarked = true;
+}
+
+void markValue(Value value) {
+  if (IS_OBJ(value))
+    markObject(AS_OBJ(value));
 }
 
 static void freeObject(Obj *object) {
@@ -58,10 +78,30 @@ static void freeObject(Obj *object) {
   }
 }
 
+static void markRoots() {
+  for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+    markValue(*slot);
+  }
+
+  for (int i = 0; i < vm.frameCount; i++) {
+    markObject((Obj *)vm.frames[i].closure);
+  }
+
+  for (ObjUpvalue *upvalue = vm.openUpvalues; upvalue != NULL;
+       upvalue = upvalue->next) {
+    markObject((Obj *)upvalue);
+  }
+
+  markTable(&vm.globals);
+  markCompilerRoots();
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
 #endif
+
+  markRoots();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
