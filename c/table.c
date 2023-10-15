8c05ee2dae88978c1636967de493111be0edf5ec
Bruno Parga <brunoparga@gmail.com>
2023-10-15 08:17:47 +0400

    [C] 30.2 Optimize hash table probing

--- c/table.c
+++ c/table.c
@@ -20,7 +20,7 @@ void freeTable(Table *table) {
 }
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry *tombstone = NULL;
   for (;;) {
     Entry *entry = &entries[index];
@@ -35,7 +35,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
       return entry;
     }
 
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 
@@ -120,7 +120,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
   if (table->count == 0)
     return NULL;
 
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & (table->capacity - 1);
   for (;;) {
     Entry *entry = &table->entries[index];
     if (entry->key == NULL && IS_NIL(entry->value)) {
@@ -130,7 +130,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
                memcmp(entry->key->chars, chars, length) == 0) {
       return entry->key;
     }
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 
