09378a9a4bf3a06612d2a29547e5752433c4dca0
Bruno Parga <brunoparga@gmail.com>
2023-10-15 15:43:44 +0400

    [java] 12.4 Set and get fields in instances

--- java/src/net/brunoparga/lox/Interpreter.java
+++ java/src/net/brunoparga/lox/Interpreter.java
@@ -224,6 +224,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name, "Only instances have properties.");
+  }
+
+  @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
   }
@@ -246,6 +256,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     return evaluate(expr.right);
+  }
+
+  @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+
+    if(!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name, "Only instances have fields.");
+    }
+
+    Object value = evaluate(expr.value);
+    ((LoxInstance)object).set(expr.name, value);
+    return value;
   }
 
   @Override
--- java/src/net/brunoparga/tool/GenerateAst.java
+++ java/src/net/brunoparga/tool/GenerateAst.java
@@ -15,8 +15,10 @@ public class GenerateAst {
     String outputDir = args[0];
     defineAst(outputDir, "Expr",
         Arrays.asList("Assign: Token name, Expr value", "Binary: Expr left, Token operator, Expr right",
-            "Call: Expr callee, Token paren, List<Expr> arguments", "Grouping: Expr expression",
+            "Call: Expr callee, Token paren, List<Expr> arguments", "Get: Expr object, Token name",
+            "Grouping: Expr expression",
             "Literal: Object value", "Logical: Expr left, Token operator, Expr right",
+            "Set: Expr object, Token name, Expr value",
             "Unary: Token operator, Expr right", "Variable: Token name"));
 
     defineAst(outputDir, "Stmt", Arrays.asList("Block: List<Stmt> statements",
