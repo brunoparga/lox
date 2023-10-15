883eff527dbcce102f82931b9d2ffd8d678d0ecc
Bruno Parga <brunoparga@gmail.com>
2023-10-15 12:32:04 +0400

    [java] 11.4 Set up the resolver to run

--- java/src/net/brunoparga/lox/Interpreter.java
+++ java/src/net/brunoparga/lox/Interpreter.java
@@ -1,11 +1,14 @@
 package net.brunoparga.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
   private Environment environment = globals;
+  private final Map<Expr, Integer> locals = new HashMap<>();
 
   Interpreter() {
     globals.define("clock", new LoxCallable() {
@@ -40,9 +43,8 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     stmt.accept(this);
   }
 
-  // Stub
   void resolve(Expr expr, int depth) {
-    return;
+    locals.put(expr, depth);
   }
 
   void executeBlock(List<Stmt> statements, Environment environment) {
@@ -131,7 +133,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Object visitAssignExpr(Expr.Assign expr) {
     Object value = evaluate(expr.value);
-    environment.assign(expr.name, value);
+
+    Integer distance = locals.get(expr);
+    if (distance == null) {
+      globals.assign(expr.name, value);
+    } else {
+      environment.assignAt(distance, expr.name, value);
+    }
+
     return value;
   }
 
@@ -199,7 +208,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Object visitVariableExpr(Expr.Variable expr) {
-    return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
+  }
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance == null) {
+      return globals.get(name);
+    } else {
+      return environment.getAt(distance, name.lexeme);
+    }
   }
 
   @Override
--- java/src/net/brunoparga/lox/Lox.java
+++ java/src/net/brunoparga/lox/Lox.java
@@ -62,6 +62,9 @@ public class Lox {
     if (hadError)
       return;
 
+    Resolver resolver = new Resolver(interpreter);
+    resolver.resolve(statements);
+
     interpreter.interpret(statements);
   }
 
