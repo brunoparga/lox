372f4edff7d6734496da1d6ef4a6556c870eb2e8
Bruno Parga <brunoparga@gmail.com>
2023-10-15 16:09:58 +0400

    [java] 12.6 Implement `this`

--- java/src/net/brunoparga/lox/Parser.java
+++ java/src/net/brunoparga/lox/Parser.java
@@ -359,13 +359,13 @@ class Parser {
       return new Expr.Literal(true);
     if (match(NIL))
       return new Expr.Literal(null);
-    if (match(NUMBER, STRING)) {
+    if (match(NUMBER, STRING))
       return new Expr.Literal(previous().literal);
-    }
+    if (match(THIS))
+      return new Expr.This(previous());
 
-    if (match(IDENTIFIER)) {
+    if (match(IDENTIFIER))
       return new Expr.Variable(previous());
-    }
 
     if (match(LEFT_PAREN)) {
       Expr expr = expression();
