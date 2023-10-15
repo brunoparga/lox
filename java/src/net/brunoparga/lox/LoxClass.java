bb42c500786c0fd28233cad9000b8bea7c47e37b
Bruno Parga <brunoparga@gmail.com>
2023-10-15 15:18:37 +0400

    [java] 12.3 Allow to create instances

--- java/src/net/brunoparga/lox/LoxClass.java
+++ java/src/net/brunoparga/lox/LoxClass.java
@@ -1,6 +1,9 @@
 package net.brunoparga.lox;
 
-class LoxClass {
+import java.util.List;
+import java.util.Map;
+
+class LoxClass implements LoxCallable {
   final String name;
 
   LoxClass(String name) {
@@ -10,5 +13,16 @@ class LoxClass {
   @Override
   public String toString() {
     return name;
+  }
+
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    LoxInstance instance = new LoxInstance(this);
+    return instance;
+  }
+
+  @Override
+  public int arity() {
+    return 0;
   }
 }
---
+++ java/src/net/brunoparga/lox/LoxInstance.java
@@ -0,0 +1,17 @@
+package net.brunoparga.lox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class LoxInstance {
+  private LoxClass klass;
+
+  LoxInstance(LoxClass klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
+}
