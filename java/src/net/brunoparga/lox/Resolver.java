d9777caae261918db359231e0e4f501f98b8684b
Bruno Parga <brunoparga@gmail.com>
2023-10-15 17:09:47 +0400

    [java] 13.3.3 Error on invalid uses of `super`

--- java/src/net/brunoparga/lox/Resolver.java
+++ java/src/net/brunoparga/lox/Resolver.java
@@ -24,6 +24,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum ClassType {
     NONE,
     CLASS,
+    SUBCLASS,
   }
 
   private ClassType currentClass = ClassType.NONE;
@@ -55,6 +56,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         Lox.error(stmt.superclass.name, "A class can't inherit from itself.");
       }
 
+      currentClass = ClassType.SUBCLASS;
       resolve(stmt.superclass);
       beginScope();
       scopes.peek().put("super", true);
@@ -203,6 +205,12 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitSuperExpr(Expr.Super expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+    } else if (currentClass != ClassType.SUBCLASS) {
+      Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+    }
+
     resolveLocal(expr, expr.keyword);
     return null;
   }
