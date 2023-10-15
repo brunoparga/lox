7491153296c7157fc86fc409073e4e1b7f250754
Bruno Parga <brunoparga@gmail.com>
2023-10-15 16:38:33 +0400

    [java] 13.1 Allow a class to inherit from another

--- java/src/net/brunoparga/tool/GenerateAst.java
+++ java/src/net/brunoparga/tool/GenerateAst.java
@@ -22,7 +22,7 @@ public class GenerateAst {
             "Unary: Token operator, Expr right", "Variable: Token name"));
 
     defineAst(outputDir, "Stmt", Arrays.asList("Block: List<Stmt> statements",
-        "Class: Token name, List<Stmt.Function> methods", "Expression: Expr expression",
+        "Class: Token name, Expr.Variable superclass, List<Stmt.Function> methods", "Expression: Expr expression",
         "Function: Token name, List<Token> params, List<Stmt> body",
         "If: Expr condition, Stmt thenBranch, Stmt elseBranch", "Print: Expr expression",
         "Return: Token keyword, Expr value", "Var: Token name, Expr initializer", "While: Expr condition, Stmt body"));
