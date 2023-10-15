eb6f500313445914907244ab9f3ff5f478736ca4
Bruno Parga <brunoparga@gmail.com>
2023-10-15 09:37:49 +0400

    [C] 30.3 Optimize Lox Value representation

--- c/value.h
+++ c/value.h
@@ -2,9 +2,51 @@
 #define clox_value_h
 
 #include "common.h"
+#include <string.h>
 
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
+
+#ifdef NAN_BOXING
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define QNAN ((uint64_t)0x7FFC000000000000)
+
+#define TAG_NIL 1   // 0b01.
+#define TAG_FALSE 2 // 0b10.
+#define TAG_TRUE 3  // 0b11.
+
+typedef uint64_t Value;
+
+#define IS_BOOL(value) (((value) | 1) == TRUE_VAL)
+#define IS_NIL(value) ((value) == NIL_VAL)
+#define IS_NUMBER(value) (((value)&QNAN) != QNAN)
+#define IS_OBJ(value) (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#define AS_BOOL(value) ((value) == TRUE_VAL)
+#define AS_NUMBER(value) valueToNum(value)
+#define AS_OBJ(value) ((Obj *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+#define OBJ_VAL(obj) (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+static inline double valueToNum(Value value) {
+  double num;
+  memcpy(&num, &value, sizeof(Value));
+  return num;
+}
+
+static inline Value numToValue(double num) {
+  Value value;
+  memcpy(&value, &num, sizeof(double));
+  return value;
+}
+
+#else
 
 typedef enum {
   VAL_BOOL,
@@ -35,6 +77,8 @@ typedef struct {
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+
+#endif
 
 typedef struct {
   int capacity;
